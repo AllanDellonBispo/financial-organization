5196c4c8c16cbe2b0244fb672343dd51fe00a7a6
Allan Bispo <allandellon.desbra@gmail.com>
2023-08-14 20:15:00 -0300

    Criação da função searchInitial que busca os extract do mês atual

--- src/hooks/useExtract.ts
+++ src/hooks/useExtract.ts
@@ -1,7 +1,20 @@
+import { api } from "../services/api"
+
 export interface Extract {
     id: number,
     date: Date,
     category: string
     title: string,
     value: number
+}
+
+// Adicionar Axios ao projeto
+export const searchInitial = async() =>{
+    let extracts;
+    await api.get(`/financial-organizational/extract/search/initial`)
+    .then(response => {
+        extracts = response.data
+    });
+
+    return extracts
 }

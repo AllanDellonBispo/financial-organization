77e1d471203079d003c96e1a5d07df7ab5667bd1
Allan Bispo <allandellon.desbra@gmail.com>
2024-10-27 18:12:51 -0300

    Código responsável por verificar se o Token expirou

--- src/contexts/LoggedUser.tsx
+++ src/contexts/LoggedUser.tsx
@@ -1,6 +1,7 @@
 // LoggedUserContext.js
-import { createContext, ReactNode, useContext, useState } from "react";
+import { createContext, ReactNode, useContext, useState, useEffect } from "react";
 import { login as apiLogin } from "../hooks/useUser";
+import * as jwt from "jwt-decode";
 
 type ContextType = {
   status: boolean;
@@ -19,8 +20,32 @@ const initialContext: ContextType = {
 export const LoggedUserContext = createContext<ContextType>(initialContext);
 
 export const LoggedUserProvider = ({ children }: { children: ReactNode }) => {
-  const [status, setStatus] = useState(false);
-  const [token, setToken] = useState("");
+  const [status, setStatus] = useState(() => {
+    const savedStatus = localStorage.getItem('status');
+    return savedStatus ? JSON.parse(savedStatus) :false;
+  });
+
+  const [token, setToken] = useState(() => {
+    return localStorage.getItem('token') || '';
+  });
+
+  useEffect(() => {
+    // Verifica a expiração do token
+    if (token) {
+      try {
+        const decodedToken: any = jwt.jwtDecode(token);
+        const currentTime = Date.now() / 1000; // Em segundos
+        if (decodedToken.exp < currentTime) {
+          logout();
+        }
+      } catch (error) {
+        console.error("Erro ao decodificar o token:", error);
+        logout();
+      }
+    }
+    localStorage.setItem("status", JSON.stringify(status));
+    localStorage.setItem("token", token);
+  }, [status, token]);
 
   const loginUser = async (email: string, password: string) => {
     try {
@@ -39,7 +64,10 @@ export const LoggedUserProvider = ({ children }: { children: ReactNode }) => {
   const logout = () => {
     setStatus(false);
     setToken("");
+    localStorage.removeItem("status");
+    localStorage.removeItem("token");
   };
+
 
   return (
     <LoggedUserContext.Provider value={{ status, token, loginUser, logout }}>
